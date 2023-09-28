b13fa709cb1bd675e6a4174a2f76fa14cacd9e8b
akhiln6 <akhiln6@gmail.com>
2023-09-28 12:03:12 +0530

    products_cotroller and routes are written and model association is given

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,50 @@
 class ProductsController < ApplicationController
+ 
   def index
+    @products = Product.all
   end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = current_user.products.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Product was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product = current_user.products.find(params[:id])
+    @product.destroy
+    redirect_to products_url, notice: 'Product was successfully destroyed.'
+  end
+
+  
+  private
+ 
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+
+
 end
