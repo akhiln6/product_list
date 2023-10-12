5f1facb868b8d16b5353537d9f987fab9c48c495
akhiln6 <akhiln6@gmail.com>
2023-10-12 11:50:08 +0530

    newly sign up users are assigned as newuser

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 
   has_many :products, dependent: :destroy
 
+
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
+
 end
